0c35eab9173341c67006e8b928d6245932c13de3
tajinder2317 <soorma2317@gmail.com>
2026-02-05 11:10:10 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeTab from "./components/HomeTab";
+import DoctorsTab from "./components/DoctorsTab";
+import HistoryTab from "./components/HistoryTab";
+import SettingTab from "./components/SettingTab";
 
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeTab} />
+          <Tab.Screen name="Doctors" component={DoctorsTab} />
+          <Tab.Screen name="History" component={HistoryTab} />
+          <Tab.Screen name="Settings" component={SettingTab} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/DoctorsTab.js
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export default function DoctorsTab() {
+  return <Text>Doctors</Text>;
+}
